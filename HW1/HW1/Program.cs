87415e98a29f241409a6a3b23767bde4896a08d1
Manjesh <manjeshreddy.puram@wsu.edu>
2022-01-25 20:55:36 -0800

    Adding code the reads from a single line

---
+++ HW1/HW1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = Console.ReadLine().Split();
+
+            int a = int.Parse(numbers[0]);
+            int b = int.Parse(numbers[1]);
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
