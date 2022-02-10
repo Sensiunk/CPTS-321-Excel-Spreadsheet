1240816d4e00a61998ac79c9e033ca8af3d721dd
Manjesh <manjeshreddy.puram@wsu.edu>
2022-02-09 23:57:16 -0800

    Adjusted some of the comments to reflect proper standards

--- HW3/HW3_Tests/TestFibonacciTextReader.cs
+++ HW3/HW3_Tests/TestFibonacciTextReader.cs
@@ -1,5 +1,5 @@
-// <copyright file="Program.cs" company="Manjesh Reddy Puram 11716685">
-// Copyright (c) Manjesh Reddy Puram 11716685. All rights reserved.
+﻿// <copyright file="TestFibonacciTextReader.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
 // </copyright>
 
 namespace HW3_Tests
@@ -7,9 +7,9 @@ namespace HW3_Tests
     using NUnit.Framework;
 
     /// <summary>
-    /// Implementation of Test Cases to check for the validaty of methods.
+    /// Implementation of Test Cases to check for the validity of methods.
     /// </summary>
-    public class Tests
+    public class TestFibonacciTextReader
     {
         /// <summary>
         /// Initialize Setup.
@@ -22,10 +22,10 @@ namespace HW3_Tests
         /// <summary>
         /// Testing to see if the number that comes in is able to compute to the right number.
         /// </summary>
-        /// <param name="testFibNum"> Takes in the number of testFibNum. </param>
+        /// <param name="testFibNumber"> Takes in the number of testFibNumber. </param>
         /// <returns> We should return the value of the nth Fibonacci number. </returns>
         [TestCase(3, ExpectedResult = 2)]
-        public int TestNormalCase(int testFibNum)
+        public int TestNormalCase(int testFibNumber)
         {
             return 0;
         }
@@ -33,10 +33,10 @@ namespace HW3_Tests
         /// <summary>
         /// Testing to see if the number that comes in is able to compute to the right number.
         /// </summary>
-        /// <param name="testFibNum"> Takes in the number of testFibNum. </param>
+        /// <param name="testFibNumber"> Takes in the number of testFibNumber. </param>
         /// <returns> We should return the value of the nth Fibonacci number. </returns>
         [TestCase(0, ExpectedResult = 0)]
-        public int TestNormalCaseForZero(int testFibNum)
+        public int TestNormalCaseForZero(int testFibNumber)
         {
             return 0;
         }
@@ -44,10 +44,10 @@ namespace HW3_Tests
         /// <summary>
         /// Testing to see if the number that comes in is able to compute to the right number.
         /// </summary>
-        /// <param name="testFibNum"> Takes in the number of testFibNum. </param>
+        /// <param name="testFibNumber"> Takes in the number of testFibNumber. </param>
         /// <returns> We should return the value of the nth Fibonacci number. </returns>
         [TestCase(1, ExpectedResult = 1)]
-        public int TestNormalCaseForOne(int testFibNum)
+        public int TestNormalCaseForOne(int testFibNumber)
         {
             return 0;
         }
@@ -55,11 +55,10 @@ namespace HW3_Tests
         /// <summary>
         /// Testing to see if the number that comes in is able to compute to the right number.
         /// </summary>
-        /// <param name="testFibNum"> Takes in the number of testFibNum. </param>
         [Test]
         public void TestExceptionCase()
         {
-            int testFibNum = -1;
+            int testFibNumber = -1;
             return;
         }
     }
