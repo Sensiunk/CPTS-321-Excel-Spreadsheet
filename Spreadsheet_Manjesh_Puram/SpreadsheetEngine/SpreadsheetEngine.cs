a1ef8a38fedd6399be5a936260ef0cf738f2d214
Manjesh <manjeshreddy.puram@wsu.edu>
2022-02-25 20:53:28 -0800

    Got the SpreadsheetCell Class setup and added the necessary rules that were asked for in the directions. I also finished step 3 since it was just adding the class library

--- Spreadsheet_Manjesh_Puram/SpreadsheetEngine/SpreadsheetEngine.cs
+++ Spreadsheet_Manjesh_Puram/SpreadsheetEngine/SpreadsheetEngine.cs
@@ -2,18 +2,117 @@
 // Copyright (c) Manjesh Reddy Puram 11716685. All rights reserved.
 // </copyright>
 
-namespace SpreadsheetEngine
+namespace CptS321
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
+    using System.Runtime.CompilerServices;
 
     /// <summary>
-    /// Spreadsheet Cell Class.
+    /// Abstract class of the Spreadsheetcell.
     /// </summary>
-    public abstract class SpreadsheetCell
+    public abstract class SpreadsheetCell : INotifyPropertyChanged
     {
+        protected int rowIndex; // Index of the row
+        protected int columnIndex; // Index of the column
+        protected string cellText; // Text in the cell
+        protected string cellValue; // Value in the cell
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="SpreadsheetCell"/> class.
+        /// </summary>
+        /// <param name="newRowIndex"> Uses the value passed in to set the value of the rowIndex. </param>
+        /// <param name="newColumnIndex"> Uses the value passed in to set the value of the columnIndex. </param>
+        public SpreadsheetCell(int newRowIndex, int newColumnIndex)
+        {
+            this.rowIndex = newRowIndex;
+            this.columnIndex = newColumnIndex;
+            this.cellText = string.Empty;
+            this.cellValue = string.Empty;
+        }
+
+        /// <summary>
+        /// This is the property changed manager.
+        /// </summary>
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+        /// <summary>
+        /// Gets the rowIndex number.
+        /// </summary>
+        public int RowIndex
+        {
+            get { return this.rowIndex; }
+        }
+
+        /// <summary>
+        /// Gets the columnIndex number.
+        /// </summary>
+        public int ColumnIndex
+        {
+            get { return this.columnIndex; }
+        }
+
+        /// <summary>
+        /// Gets or sets the value of the cellText.
+        /// Sets the value if only the value is different.
+        /// </summary>
+        public string CellText
+        {
+            get
+            {
+                return this.cellText;
+            }
+
+            set
+            {
+                if (this.cellText == value)
+                {
+                    return;
+                }
+                else
+                {
+                    this.cellText = value;
+
+                    this.OnPropertyChanged("CellText");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the value of the cellValue.
+        /// In order to set the value, it must be internal and protected and only gets changed if it is not the same.
+        /// </summary>
+        public string CellValue
+        {
+            get
+            {
+                return this.cellValue;
+            }
+
+            protected internal set
+            {
+                if (this.cellValue == value)
+                {
+                    return;
+                }
+                else
+                {
+                    this.cellValue = value;
+
+                    this.OnPropertyChanged("CellValue");
+                }
+            }
+        }
+
+        /// <summary>
+        /// This is needed in order to invoke the property change.
+        /// </summary>
+        /// <param name="name"> Takes the name of the things being changed and broadcasts it. </param>
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
